24ebb00c274454b52a4a737eaa99d567dc43d3ae
JoãoPedrinho013 <joaopedroliveira98@gmail.com>
2024-04-11 19:05:38 -0300

    Initial commit

---
+++ Header&Footer.js
@@ -0,0 +1,23 @@
+// Função para carregar o header
+function CarregarCabecalho() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('header').innerHTML = data
+        })
+}
+
+// Função para carregar o footer
+function CarregarRodape() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('footer').innerHTML = data
+        })
+}
+
+// Chamar as funções para carregar o header e o footer
+document.addEventListener('DOMContentLoaded', function () {
+    CarregarCabecalho()
+    CarregarRodape()
+})
---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto-Site-Dudis-2
+ BUFALO STORE 2

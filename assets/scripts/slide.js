8312c90e710fc8e171b4e675d86fa50718f777d4
JoãoPedrinho013 <joaopedroliveira98@gmail.com>
2024-04-15 20:34:27 -0300

    Update slide.js

--- assets/scripts/slide.js
+++ assets/scripts/slide.js
@@ -7,20 +7,27 @@ const navNextButton = document.querySelector('[data-slide="nav-next-button"]')
 const controlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
 const slideItems = document.querySelectorAll('[data-slide="item"]')
 const controlButtons = document.querySelectorAll('[data-slide="control-button"]')
+let startingPoint = 0
+let savedPosition = 0
+let currentPoint = 0
+let movement = 0
 
 
 function onMouseDown(event) {
     const slideItem = event.currentTarget
+    startingPoint = event.clientX
+    currentPoint = startingPoint - savedPosition
     slideItem.addEventListener('mousemove', onMouseMove)
-    console.log('apertei o botao')
 }
 function onMouseMove(event) {
-    console.log('movi o mouse')
+    movement = event.clientX - startingPoint
+    const position = event.clientX - currentPoint
+    slideList.style.transform = 'translateX(' + position + 'px)'
+    savedPosition = position
 }
 function onMouseUp(event) {
     const slideItem = event.currentTarget
     slideItem.removeEventListener('mousemove', onMouseMove)
-    console.log('soltei o botao')
 }
 
 

3ed411908ebc20d72301cedefced77598802cdf7
Caio Silva <136451281+CaioEdimarSantosSilva@users.noreply.github.com>
2024-06-07 19:57:08 -0300

    Products

--- assets/scripts/products.js
+++ assets/scripts/products.js
@@ -1,21 +1,3 @@
-
-// O JavaScript pode ser usado para ajustar dinamicamente o tamanho das linhas
-window.addEventListener('resize', function () {
-    adjustLines();
-});
-
-function adjustLines() {
-    const containerWidth = document.querySelector('.titulo').offsetWidth;
-    const lineWidth = (containerWidth - document.querySelector('.centered-text').offsetWidth) / 2;
-    document.querySelector('.left-line').style.width = lineWidth + 'px';
-    document.querySelector('.right-line').style.width = lineWidth + 'px';
-}
-
-// Chame a função para ajustar as linhas quando a página é carregada inicialmente
-adjustLines();
-
-
-
 // Lista de produtos
 const products = [
     {
